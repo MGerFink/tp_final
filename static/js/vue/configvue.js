d09e561f5de3f1289cf13e724e44932d79133ef8
MGerFink <m-fink@hotmail.com>
2023-07-06 18:26:10 -0300

    cambios en templates/page/sales.html

--- static/js/vue/configvue.js
+++ static/js/vue/configvue.js
@@ -1,12 +1,8 @@
 const config = {
     URI: "https://sheetdb.io/api/v1/qd0tyv7l7bycu",
     PARENT_ID: "#ProdApp",
-<<<<<<< HEAD
     LOCAL:"/static/DB/prodl.json"
 
-=======
-    LOCAL:"static/DB/prodl.json"
->>>>>>> 0a3ffa6dde90a37aaf13e2a3fb18cfb0eb54f004
 };
 
 export  { config };
